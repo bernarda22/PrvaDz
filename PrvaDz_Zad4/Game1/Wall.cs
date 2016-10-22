a30045d6c3f9462cd2570ae6c34c121153f430a4
bernarda <bernarda.busic22@hotmail.com>
2016-10-22 18:34:37 +0200

    added zadatak4

---
+++ PrvaDz_Zad4/Game1/Wall.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Game1
+{
+    public interface IPhysicalObject2D
+    {
+        float X { get; set; }
+        float Y { get; set; }
+        int Width { get; set; }
+        int Height { get; set; }
+    }
+
+    public class CollisionDetector
+    {
+        public static bool Overlaps(IPhysicalObject2D a, IPhysicalObject2D b)
+        {
+            Rectangle aRect = new Rectangle((int)a.X, (int)a.Y, a.Width, a.Height);
+            Rectangle bRect = new Rectangle((int)b.X, (int)b.Y, b.Width, b.Height);
+            return aRect.Intersects(bRect);
+        }
+    }
+
+    public class Wall : IPhysicalObject2D
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Wall(float x, float y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+    }
+}
